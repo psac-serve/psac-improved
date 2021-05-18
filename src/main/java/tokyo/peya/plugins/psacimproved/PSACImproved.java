6c67e8e9d4cc5e72ab2a58e22ac9d462a37d3e30
Peyang <42040068+peyang-Celeron@users.noreply.github.com>
2021-05-18 23:38:01 +0900

    feat: database

--- src/main/java/tokyo/peya/plugins/psacimproved/PSACImproved.java
+++ src/main/java/tokyo/peya/plugins/psacimproved/PSACImproved.java
@@ -1,15 +1,30 @@
 package tokyo.peya.plugins.psacimproved;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import static tokyo.peya.plugins.psacimproved.Variables.*;
 
 public final class PSACImproved extends JavaPlugin
 {
 
+
+    public PSACImproved()
+    {
+        saveDefaultConfig();
+        config = getConfig();
+        
+        HikariConfig databaseConfig = new HikariConfig();
+        databaseConfig.setJdbcUrl(config.getString("database.jdbc"));
+        databaseConfig.setDriverClassName(config.getString("database.driver"));
+        
+        dataSource = new HikariDataSource(databaseConfig);
+    }
+
     @Override
     public void onEnable()
     {
-        // Plugin startup logic
-
     }
 
     @Override
---
+++ src/main/java/tokyo/peya/plugins/psacimproved/Variables.java
@@ -0,0 +1,10 @@
+package tokyo.peya.plugins.psacimproved;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class Variables
+{
+    public static FileConfiguration config;
+    public static HikariDataSource dataSource;
+}
